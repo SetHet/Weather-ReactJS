f000eee3c6986d1bb14cee52dcc766cfa46c673b
SetHet <sethet1@gmail.com>
2024-03-26 22:07:37 -0300

    Weather Datail by hours

--- src/Components/WeatherDayDetail/index.js
+++ src/Components/WeatherDayDetail/index.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "./style.css";
 import { WeatherHourDetail } from "../WeatherHourDetail";
+import { WeatherContext } from "../../Contexts/WeatherContext";
+
+function addTime(date, hours) {
+  const hour = date.getHours() + hours;
+  const newDate = new Date(date);
+  newDate.setHours(hour);
+  //console.log(hours);
+  //console.log(newDate);
+  return newDate;
+}
 
 function WeatherDayDetail() {
+  const { data } = React.useContext(WeatherContext);
+
   return (
     <div className="containerDayDetail">
-      <WeatherHourDetail />
-      <WeatherHourDetail />
-      <WeatherHourDetail />
-      <WeatherHourDetail />
+      {data.hourly ? (
+        data.hourly.time
+          .filter((t) => {
+            const time = new Date(t);
+            //console.log(new Date());
+            const now = addTime(new Date(), -1);
+            const in12hr = addTime(new Date(), 12);
+            // console.log(time);
+            // console.log(`${time}
+            // ${now}
+            // ${in12hr}`);
+            return time > now && time < in12hr;
+          })
+          .map((t, i) => <WeatherHourDetail date={t} key={i} />)
+      ) : (
+        <div>Nada</div>
+      )}
     </div>
   );
 }
--- src/Components/WeatherHourDetail/index.js
+++ src/Components/WeatherHourDetail/index.js
@@ -1,14 +1,47 @@
 import React from "react";
 import "./style.css";
+import { WeatherContext } from "../../Contexts/WeatherContext";
 
-function WeatherHourDetail() {
+function WeatherHourDetail({ date }) {
+  const { data } = React.useContext(WeatherContext);
+  const index = data.hourly.time.findIndex((d) => d === date);
+  const dataTime = new Date(date);
+  const temperature = data.hourly.temperature_2m[index];
+  const temperatureUnit = data.hourly_units.temperature_2m;
+  const windDirection = data.hourly.wind_direction_10m[index];
+  const windSpeed = data.hourly.wind_speed_10m[index];
+  const windUnit = data.hourly_units.wind_speed_10m;
+  const precipitationProbability = data.hourly.precipitation_probability[index];
+  const precipitationProbabilityUnit =
+    data.hourly_units.precipitation_probability;
+  const rain = data.hourly.rain[index];
+  const rainUnit = data.hourly_units.rain;
+
   return (
-    <div className="containerHourDetail">
-      <div>00am</div>
-      <div>25*C</div>
-      <div>45* 20m/s</div>
-      <div>0mm</div>
-    </div>
+    <>
+      <div className="containerHourDetail">
+        <div>
+          {dataTime.getDate()}/{dataTime.getMonth()} {dataTime.getHours()}hr
+        </div>
+        <div>
+          {temperature} {temperatureUnit}
+        </div>
+        <div className="windContainer">
+          <div className="direccion">
+            <img src="/direccioncardinal.png" className="dirCardinal" />
+            <img
+              src="/ArrowNorth.png"
+              className="dirArrow"
+              style={{ transform: `rotate(${windDirection}deg)` }}
+            />
+          </div>{" "}
+          {windSpeed} {windUnit}
+        </div>
+        <div>
+          {rain} {rainUnit}
+        </div>
+      </div>
+    </>
   );
 }
 
