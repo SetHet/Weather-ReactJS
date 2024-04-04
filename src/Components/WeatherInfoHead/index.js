23be1eaec141165e8e2101dc8a842f215f5887a9
SetHet <36393893+SetHet@users.noreply.github.com>
2024-04-04 19:39:15 -0300

    Head Info source code link

--- src/Components/WeatherInfoHead/index.js
+++ src/Components/WeatherInfoHead/index.js
@@ -6,8 +6,15 @@ function WeatherInfoHead() {
     <div className="containerWeatherInfoHead">
       <h3 className="WeatherInfoHead">
         Thanks to the weather API{" "}
-        <a href="https://open-meteo.com/">open-meteo.com</a> ❤️
+        <a href="https://open-meteo.com/">open-meteo.com❤️</a>
       </h3>
+      <h4 className="WeatherInfoHead">
+        See the source code on{" "}
+        <a href="https://github.com/SetHet/Weather-ReactJS">
+          GitHub{" "}
+          <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" />
+        </a>
+      </h4>
     </div>
   );
 }
